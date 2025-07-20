736a86dfe978c509055543c4498cb7ed480b26e3
Phan Hữu Gia Huy <88171094+Aidesx@users.noreply.github.com>
2025-07-20 16:57:07 +0700

    Delete game project directory

--- game project/level/level5.py
+++
@@ -1,7 +0,0 @@
-level_number = 5
-enemy_speed = 1.4
-enemy_spawn_rate = 15
-max_enemies = 15
-shoot_cooldown = 500
-background_color = (220, 220, 255)
-#background_image_path = "assets/bg1.jpg" 
--- game project/project-python/game project/level/level1.py
+++
@@ -1,8 +0,0 @@
-# level1.py
-level_number = 1
-enemy_speed = 1
-enemy_spawn_rate = 25
-max_enemies = 10
-shoot_cooldown = 500
-background_color = (240, 240, 240)
-#background_image_path = "assets/bg1.jpg" 
