9600fba4199ede6cd350d75eb8fd895b463a2d8e
Phan Hữu Gia Huy <88171094+Aidesx@users.noreply.github.com>
2025-07-20 16:57:26 +0700

    Delete game project directory

--- game project/level/__init__.py
+++
@@ -1,2 +0,0 @@
-# đanh giá đây là một package để chứa các module liên quan đến cấp độ trong trò chơi
-# có thể ko ghi giới thiệu gì ở đây, chỉ cần để trống hoặc ghi chú ngắn gọn
--- game project/level/level3.py
+++
@@ -1,7 +0,0 @@
-level_number = 3
-enemy_speed = 1.2
-enemy_spawn_rate = 25
-max_enemies = 12
-shoot_cooldown = 500
-background_color = (220, 220, 255)
-#background_image_path = "assets/bg1.jpg" 
